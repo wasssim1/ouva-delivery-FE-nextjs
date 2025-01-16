8e4c9c69f3ed840bbf90fde49cb293afb8e992ac
Wassim Mhamdi <mh.wassim1@gmail.com>
2025-01-16 11:20:48 +0100

    Connect zone and store pages to backend

--- src/app/[locale]/zone/[slug]/page.tsx
+++ src/app/[locale]/zone/[slug]/page.tsx
@@ -6,13 +6,20 @@ import LayoutContainer from "@/components/LayoutContainer";
 import Navbar from "@/components/navbar/Navbar";
 import { ZonePageInteractiveWrapper } from "@/components/page-zone/ZonePageInteractiveWrapper";
 
-import { RESTAURANTS_LIST_DATA } from "@/data/restaurants";
-import { STORE_CATEGORIES } from "@/data/store-categories";
-import { ZONES_LIST_DATA } from "@/data/zones";
-import Head from "next/head";
+import { FoodStore } from "@/interfaces/food-store.interface";
+import { Zone } from "@/interfaces/zone.interface";
 
 export async function generateStaticParams() {
-  return ZONES_LIST_DATA.map((zone) => ({
+  const zonesResp: Zone[] = await fetch(
+    `${process.env.NEXT_PUBLIC_OUVA_API_URL}/zones/`
+  ).then((res) => res.json());
+
+  if (!zonesResp) {
+    console.error("Error fetching zones data - static params");
+    return [];
+  }
+
+  return zonesResp.map((zone) => ({
     params: { slug: zone.slug },
   }));
 }
@@ -22,9 +29,11 @@ export async function generateMetadata({
 }: {
   params: { slug: string };
 }): Promise<Metadata> {
-  const zoneData = ZONES_LIST_DATA.find((zone) => zone.slug === params.slug);
-
+  const zoneData: Zone = await fetch(
+    `${process.env.NEXT_PUBLIC_OUVA_API_URL}/zones/${params.slug}/`
+  ).then((res) => res.json());
   if (!zoneData) {
+    console.error("Error fetching zone data - metadata");
     return {
       title: "Zone not found | Ouva Delivery",
       description: "Zone not found",
@@ -37,36 +46,69 @@ export async function generateMetadata({
   };
 }
 
-const Page = ({ params }: { params: { slug: string } }) => {
-  const zoneData = ZONES_LIST_DATA.find((zone) => zone.slug === params.slug);
+const Page = async ({ params }: { params: { slug: string } }) => {
+  const zoneData = await fetch(
+    `${process.env.NEXT_PUBLIC_OUVA_API_URL}/zones/${params.slug}/`
+  )
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error("Error fetching zone data - slug", { err });
+      notFound();
+    });
 
   if (!zoneData) {
     notFound();
   }
 
-  const availableStoresListData = RESTAURANTS_LIST_DATA.filter(
-    (store) => store.zone === zoneData.slug
-  );
+  const storesListDataResp = await fetch(
+    `${process.env.NEXT_PUBLIC_OUVA_API_URL}/food-stores?zone=${params.slug}`
+  )
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error("Error fetching stores data - slug", { err });
+    });
+  const availableStoresListData = storesListDataResp.ouvaStores;
 
-  const availableStoreCategoriesData = STORE_CATEGORIES.filter((category) =>
-    availableStoresListData.some((store) =>
-      store.categories.includes(category.slug)
-    )
-  );
+  const categoriesIdsSet = new Set();
+  availableStoresListData.forEach((store: FoodStore) => {
+    store.categories.forEach((categId) => {
+      categoriesIdsSet.add(categId);
+    });
+  });
+  const storeCategoriesDataResp = await fetch(
+    `${process.env.NEXT_PUBLIC_OUVA_API_URL}/store-categories/find?ids=${[
+      ...categoriesIdsSet,
+    ].join(",")}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error("Error fetching store categories data - slug", { err });
+    });
+
+  const availableStoreCategoriesData = storeCategoriesDataResp.categoriesList;
+  // TODO: filter categories based on available stores
+  // STORE_CATEGORIES.filter((category) =>
+  //   availableStoresListData.some((store) =>
+  //     store.categories.includes(category.slug)
+  //   )
+  // );
 
   return (
     <>
-      <Head>
-        <title>{`${zoneData.name} | Ouva Delivery`}</title>
-      </Head>
-
       <LayoutContainer>
         <Navbar />
 
         <ZonePageInteractiveWrapper
           zoneData={zoneData}
-          storeCategoriesData={availableStoreCategoriesData}
-          storesListData={availableStoresListData}
+          // storeCategoriesData={availableStoreCategoriesData || []}
+          storeCategoriesData={[]}
+          storesListData={availableStoresListData || []}
         />
 
         <Footer />
@@ -77,4 +119,4 @@ const Page = ({ params }: { params: { slug: string } }) => {
 
 export default Page;
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
