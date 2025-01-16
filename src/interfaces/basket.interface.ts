8fb4b5baff90fad1703cba45f5a066c1d8f15c88
Wassim Mhamdi <mh.wassim1@gmail.com>
2025-01-16 10:48:51 +0100

    Update store and basket interfaces

--- src/interfaces/basket.interface.ts
+++ src/interfaces/basket.interface.ts
@@ -1,23 +1,56 @@
-import {
-  ExtraIngredient,
-  FoodStore,
-  MenuItem,
-  MenuItemOption,
-} from "./food-store.interface";
+export interface BasketItemOptionGroup {
+  optionKey: string;
+  optionValueSlug: string;
+}
 
 export interface BasketItem {
-  basketItemKey: string;
-  itemDetails: MenuItem;
-  baseUnitPrice: number;
-  finalUnitPrice: number;
-  selectedOption: MenuItemOption;
-  selectedExtras: ExtraIngredient[];
+  basketItemKey?: string; // for edit mode only
+  // menuItemInfo: MenuItem; // TODO: check if needed to calculate basket item price in FE
+  menuItemSlug: string;
+  unitPrice: number;
+  selectedOptions: BasketItemOptionGroup[];
+  selectedExtrasSlugs?: string[];
   quantity: number;
   note?: string;
 }
 
 export interface BasketState {
-  foodStore: FoodStore;
-  orderItems: BasketItem[];
-  //   totalPrice: number;
+  basketStorageKey?: string;
+  foodStoreSlug: string;
+  basketItems: BasketItem[];
+  totalPrice: number;
+}
+
+export interface UpsertBasketItemRequestDto {
+  basketStorageKey?: string; // only for update
+  quantity: number;
+  unitPrice: number;
+  selectedOptions: { optionKey: string; optionValueSlug: string }[];
+  selectedExtrasSlugs?: string[];
+  menuItemSlug: string;
+  note?: string;
+
+  // relations
+  foodStoreSlug: string;
+  userId?: string;
+}
+
+export interface UpsertBasketItemResponseDto {
+  basketStorageKey: string;
+  basketItemsCount: number;
+  basketItems: {
+    basketItemKey: string;
+    quantity: number;
+    unitPrice: number;
+    selectedOptions: {
+      optionKey: string;
+      optionValueSlug: string; // slug or whole object?
+    }[];
+    selectedExtrasSlugs?: string[];
+    menuItemSlug: string;
+    note?: string;
+  }[];
+  totalAmount: number;
+  foodStoreSlug: string;
+  userId?: string;
 }
